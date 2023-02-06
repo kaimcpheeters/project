96b3b1add30ff80abf7dac9d6912442287017985
Kai McPheeters <kaimcpheeters@gmail.com>
2023-02-06 15:19:47 -0500

    Added fles to view

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from app import views
+
+def index(request):
+    return "Hello World"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('-/', include('django_alive.urls')),
 
 ]
